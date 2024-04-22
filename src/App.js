3210d41197fff01ed569302fc50bdf317daf8fd5
mahesh_sv <memaheshwaran@gmail.com>
2024-04-22 16:01:46 +0530

    Webpack Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "30px",
+        backgroundColor: "#60f0f0",
+      }}
+    >
+      Mahesh Webpack Configuration
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: "index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+};
